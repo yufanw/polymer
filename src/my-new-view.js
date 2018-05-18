84648476ec4a15b01f255a69184bb3e01c4ccd66
Yufan Wang <yufanwangcn@hotmail.com>
2018-05-18 15:04:25 -0700

    added info to polymer page

--- src/my-new-view.js
+++ src/my-new-view.js
@@ -17,9 +17,19 @@ class MyNewView extends PolymerElement {
         <h1>Polymer</h1>
         <img src="/images/manifest/icon-192x192.png">
         <p>
-          An ultra-light, highly performant custom element base class 
-          with a simple but expressive API.
+          The Polymer library provides a set of features for creating custom elements.
+          These features are designed to make it easier and faster to make custom elements that 
+          work like standard DOM elements. 
+          Similar to standard DOM elements, Polymer elements can be:
         </p>
+        <ul>
+          <li>Instantiated using a constructor or document.createElement.</li>
+          <li>Configured using attributes or properties.</li>
+          <li>Populated with internal DOM inside each instance.</li>
+          <li>Responsive to property and attribute changes.</li>
+          <li>Styled with internal defaults or externally.</li>
+          <li>Responsive to methods that manipulate its internal state.</li>
+        </ul>  
       </div>
     `;
   }
